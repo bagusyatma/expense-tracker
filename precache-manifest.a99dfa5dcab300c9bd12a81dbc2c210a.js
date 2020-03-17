8c6e3365aed4f17660a79a2dc662d6e1a2086c7d
bagusyatma <bagusyatma1@gmail.com>
2020-03-17 14:51:35 +0700

    Updates

---
+++ precache-manifest.a99dfa5dcab300c9bd12a81dbc2c210a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f5ac59112a39cb2b634675440bb1c6",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "85423944f5875b52a049",
+    "url": "/expense-tracker/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "2b82d178e53d0d63af8e",
+    "url": "/expense-tracker/static/js/2.24e7ceeb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/expense-tracker/static/js/2.24e7ceeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85423944f5875b52a049",
+    "url": "/expense-tracker/static/js/main.a7b749a7.chunk.js"
+  },
+  {
+    "revision": "b8a83865aa99db9bf575",
+    "url": "/expense-tracker/static/js/runtime-main.1b8cc902.js"
+  }
+]);
